fd4368ef8ed7647883834e428dee8f69faedabef
weebney <weebney@gmail.com>
2024-12-06 13:40:51 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Far Side Daily Dose Bot (Bluesky)](https://bsky.app/profile/bot.cowtools.org)
+
+This bot posts selections directly from [https://www.thefarside.com/] 5 times per day, everyday. It is written in TypeScript via Deno.
---
+++ main.ts
@@ -0,0 +1,50 @@
+import { AtpAgent } from "npm:@atproto/api";
+import "jsr:@std/dotenv/load";
+import { DOMParser } from "jsr:@b-fuze/deno-dom";
+
+const count = +Deno.env.get("COUNT")!;
+
+async function main() {
+  try {
+    const response = await fetch("https://www.thefarside.com/");
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html")!;
+    const card = doc.querySelectorAll(".card-body")![count % 5];
+    const text = card.querySelector(".figure-caption")!.innerText.trim();
+    const imageUrl = card.querySelector("img")!.getAttribute("data-src")!;
+    const imageResponse = await fetch(imageUrl);
+    const imageBlob = await imageResponse.blob();
+
+    const agent = new AtpAgent({
+      service: "https://bsky.social",
+    });
+
+    await agent.login({
+      identifier: Deno.env.get("BLUESKY_USERNAME")!,
+      password: Deno.env.get("BLUESKY_PASSWORD")!,
+    });
+
+    const blobResp = await agent.uploadBlob(imageBlob);
+
+    console.log(blobResp);
+
+    await agent.post({
+      text: text,
+      embed: {
+        $type: "app.bsky.embed.images",
+        images: [{
+          image: blobResp.data.blob,
+          alt: text,
+        }],
+      },
+    });
+    console.log("Just posted!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
+
+await Deno.writeTextFile("state", `${count + 1}`);
